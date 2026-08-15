069fe69675b70ffe4ef60267b9188acfb128271c
Ali Baker <alisondre.baker@gmail.com>
2026-08-15 17:40:11 -0400

    Redirect root URL directly to Swagger documentation

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,16 @@
+from flask import redirect
 from app import create_app
 from app.models import db
 
 app = create_app('ProductionConfig')
-
   
 with app.app_context():
     # db.drop_all()
     db.create_all()
+    
+@app.route('/')
+def home():
+    return redirect('/api/docs/')
     
 if __name__=='__main__':
     app.run()
